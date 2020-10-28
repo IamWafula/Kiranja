4dbf8f46dd3154226635221386a9da3d377e2b8a
IamWafula <46840113+IamWafula@users.noreply.github.com>
2020-10-28 16:54:13 +0300

    Add files via upload

---
+++ icrv=club/trial.py
@@ -0,0 +1,10 @@
+print("Enter the first number:")
+f_num = int(input())
+
+print("Enter the second number:")
+s_num = int(input())
+
+subtraction = f_num - s_num
+
+print ("The value is:")
+print ( subtraction )
